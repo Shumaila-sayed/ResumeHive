77e5837dfccfd79af5b1c4816eee9963cd0dce0c
Shumaila <sayedshumaila27@gmail.com>
2025-03-02 03:22:41 +0530

    Style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,28 @@
+import '../styles/Header.css'
+import downloadIcon from '../assets/download_32dp_FFFFFF_FILL0_wght400_GRAD0_opsz40.svg';
+
 function Header() {
     return (
-			<div>
+			<div className='header'>
 				<h1>ResumeHive</h1>
-				<button>Download as pdf</button>
-				<p>
-					Created by{' '}
-					<a href='https://github.com/Shumaila-sayed' target="_blank" >Shumaila Sayed</a>
-				</p>
+				<div className='header-info'>
+					<button>
+						<img
+							src={downloadIcon}
+							alt=''
+						/>
+						Download as pdf
+					</button>
+					<p>
+						Created by{' '}
+						<a
+							href='https://github.com/Shumaila-sayed'
+							target='_blank'
+						>
+							Shumaila Sayed
+						</a>
+					</p>
+				</div>
 			</div>
 		);
 }
